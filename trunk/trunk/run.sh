bf6d5dcf1b8a35a697578ec479b0d2fdf97511ec
yglodt <yglodt@5fafe11d-bc4c-0410-828b-6393e24f7932>
2008-05-10 00:06:12 +0000

    remove

--- trunk/trunk/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd bin
-ln -sf ../fbdaogenerator.ini
-export CLASSPATH=.:../lib/jaybird-full-2.1.3.jar
-
-java Main
-
-cd ..
--- trunk/trunk/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd bin
-
-export CLASSPATH=.:../lib/jaybird-full-2.1.3.jar
-
-java Test
-
-cd ..
