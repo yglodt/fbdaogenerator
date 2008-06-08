52d95cacad17d45ac2eef88c20fd915e2194bfb9
yglodt <yglodt@5fafe11d-bc4c-0410-828b-6393e24f7932>
2008-06-08 19:08:07 +0000

    start to use JarBuilder for creating the package

--- fbDaoGenerator/run.sh
+++ fbDaoGenerator/run.sh
@@ -2,7 +2,7 @@
 
 cd bin
 ln -sf ../fbdaogenerator.ini
-export CLASSPATH=.:../lib/jaybird-full-2.1.3.jar
+export CLASSPATH=.:../lib/jaybird-full-2.1.3.jar:../lib/ant.jar:/usr/lib/jvm/java-6-sun-1.6.0.06/lib/tools.jar
 
 java Main
 
