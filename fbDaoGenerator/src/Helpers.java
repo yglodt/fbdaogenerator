e59c22ed204bc9b899232fc37712b27bef88287b
yglodt <yglodt@5fafe11d-bc4c-0410-828b-6393e24f7932>
2008-06-30 18:24:37 +0000

    factor out code from Main to JavaSpecific, no functional change

--- fbDaoGenerator/src/Helpers.java
+++ fbDaoGenerator/src/Helpers.java
@@ -1,3 +1,5 @@
+import java.io.File;
+
 
 public class Helpers {
 
@@ -24,6 +26,20 @@ public class Helpers {
 			}
 		}
 		return s.toString();
+	}
+
+	public static boolean deleteDir(File dir) {
+		if (dir.isDirectory()) {
+			String[] children = dir.list();
+			for (int i = 0; i < children.length; i++) {
+				boolean success = deleteDir(new File(dir, children[i]));
+				if (!success) {
+					return false;
+				}
+			}
+		}
+		// The directory is now empty so delete it
+		return dir.delete();
 	}
 
 }
