e4af2b36dc09ec911118e6f25cda881025e61116
yglodt <yglodt@5fafe11d-bc4c-0410-828b-6393e24f7932>
2008-06-30 18:28:30 +0000

    use method from Helpers, don't duplicate

--- fbDaoGenerator/src/DataFieldFirebird.java
+++ fbDaoGenerator/src/DataFieldFirebird.java
@@ -92,36 +92,12 @@ public class DataFieldFirebird {
 		this.isInPK = isInPK;
 	}
 	
-	public static String underscoreSeparatedToCamelCase(String name) {
-		// copied from: http://www.mail-archive.com/dev@commons.apache.org/msg03457.html
-		name = name.toLowerCase();
-        StringBuilder s = new StringBuilder();
-        if (name == null) {
-                return "";
-        }
-        int length = name.length();
-        boolean upperCase = false;
-
-        for (int i = 0; i < length; i++) {
-                char ch = name.charAt(i);
-                if (ch == '_') {
-                        upperCase = true;
-                } else if (upperCase) {
-                        s.append(Character.toUpperCase(ch));
-                        upperCase = false;
-                } else {
-                        s.append(ch);
-                }
-        }
-        return s.toString();
-	}
-
 	public String getJavaName() {
 		return javaName;
 	}
 
 	public void setJavaName(String javaName) {
-		javaName = underscoreSeparatedToCamelCase(javaName);
+		javaName = Helpers.underscoreSeparatedToCamelCase(javaName);
 		String temp = JavaSpecific.replaceKeyWords(javaName);
 		if (javaName.equals(temp)) {
 			this.javaName = javaName;
