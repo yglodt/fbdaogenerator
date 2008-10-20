011700b2d5a2d56960f81749d1c39a12abfe3559
yglodt <yglodt@5fafe11d-bc4c-0410-828b-6393e24f7932>
2008-10-20 21:31:10 +0000

    new jdbc

--- fbDaoGenerator/build.bat
+++ fbDaoGenerator/build.bat
@@ -1,6 +1,6 @@
 cd bin
 
-set CLASSPATH=.;..\lib\jaybird-full-2.1.5.jar;..\lib\ant.jar;C:\Progra~1\Java\jdk1.6.0_06\lib\tools.jar
+set CLASSPATH=.;..\lib\jaybird-full-2.1.6.jar;..\lib\ant.jar;C:\Progra~1\Java\jdk1.6.0_06\lib\tools.jar
 javac ..\src\*.java
 
 copy ..\src\*.class .
