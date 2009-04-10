2b565f00d2cc8e27bcc86b6a2ab2c1ac25335575
yglodt <yglodt@5fafe11d-bc4c-0410-828b-6393e24f7932>
2009-04-10 10:26:01 +0000

    update path to reflect new jaybird

--- trunk/fbDaoGenerator/build.sh
+++ trunk/fbDaoGenerator/build.sh
@@ -2,7 +2,7 @@
 
 cd bin
 
-CLASSPATH=.:../lib/jaybird-full-2.1.3.jar:../lib/ant.jar:/usr/lib/jvm/java-6-sun/lib/tools.jar javac -verbose ../src/*.java 
+CLASSPATH=.:../lib/jaybird-full-2.1.6.jar:../lib/ant.jar:/usr/lib/jvm/java-6-sun/lib/tools.jar javac -verbose ../src/*.java 
 mv ../src/*.class .
 
 cd ..
